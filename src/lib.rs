f5c44fb708c8a551cf80d03bd7ccd361bbad5c29
Cawolf <camilozambranovotto@hotmail.com>
2024-01-06 00:04:34 -0500

    Feat: basic read from "asm" file :godmode:
    I added some simple skeleton for file reading and getting the
    instructions to emulate when running the CPU. This is temporary as
    I learn more and we add more to the emulator. Once we start reading from
    ACTUAL rom files, we would need to update all of this.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,38 @@
 use std::error::Error;
+use std::fs;
 
 mod cpu;
 mod controller;
 
 use cpu::Cpu;
-use controller::{Controller, ControllerState};
+use controller::{Controller, ControllerState}; //TODO: we should not bring the enum into scope like this, but we can leave it for now while we add more functionality
 
 const INSTRUCTIONS: &str =
     "lda #42\nsta $0015\nnotAnInstruction\nldx #23\nldy #69\nlda 0\nlda $FF\nnop";
 
-pub fn run() -> Result<(), Box<dyn Error>> {
+#[derive(Default)]
+pub struct Config {
+    pub rom_path: Option<String>,
+}
+
+impl Config {
+    pub fn new(rom_path: Option<String>) -> Self{
+        Self {
+            rom_path,
+            ..Default::default()
+        }
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //The ram should be it's own structure to be honest. I'll keep it like this for now
     let mut ram: [u8; 2048] = [0; 2048];
 
     let mut cpu = Cpu::new();
 
-    for line in INSTRUCTIONS.lines() {
+    let instructions = get_instructions(&config.rom_path);
+
+    for line in instructions.lines() {
         let mut iter = line.split(' ');
         
         let Some(instruction) = iter.next() else {
@@ -38,4 +55,26 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     println!("Controller state is {:08b}", controller1.get_controller_state());
 
     Ok(())
+}
+
+fn get_instructions(rom_path: &Option<String>) -> String {
+    if let Some(ref path) = rom_path {
+        println!("Reading from file {path}");
+
+        if let Ok(file_contents) = read_rom_file(path) {
+           return file_contents;
+        }
+
+        println!("we couldn't read from file.");
+
+    }
+
+    println!("Using internal instruction test");
+
+    INSTRUCTIONS.to_string()
+}
+
+pub fn read_rom_file(path: &str) -> Result<String, Box<dyn Error>> {
+    let contents = fs::read_to_string(path)?;
+    Ok(contents)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+use std::env;
 use std::process;
 
+use rusty_nes::Config;
+
 fn main() {
-    let emulation_result = rusty_nes::run();
+    let config: Config = create_config();
+
+    let emulation_result = rusty_nes::run(config);
 
     if let Err(e) = emulation_result {
         eprintln!("Emulation error triggered!!");
         eprintln!("The error encountered was: {e}");
         process::exit(1);
     }
+}
+
+fn create_config() -> Config {
+    let args: Vec<String> = env::args().collect();
+
+    let rom_path = match args.get(1) {
+        Some(ref path) => Some(path.to_string()),
+        None => None
+    };
+
+    Config::new(rom_path)
 }
